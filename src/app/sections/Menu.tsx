c7ed6640dc930513d7445b00819a886c1f39e284
justin <justin.ng26@hotmail.com>
2024-03-07 16:53:26 +0000

    building out menu component

--- src/app/sections/Menu.tsx
+++ src/app/sections/Menu.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { filters } from "../data/data";
 import "../styles/menu.css";
+import SectionTitle from "../components/SectionTitle";
+import MenuItem from "../components/MenuItem";
 
 export default function Menu() {
   const [data, setData] = useState([]);
@@ -23,5 +26,51 @@ export default function Menu() {
   useEffect(() => {
     setItems(data);
   }, [data]);
-  return <div>Menu</div>;
+
+  const handleFilterChange = (id: number, category: string) => {};
+
+  return (
+    <section id="menu" className="menu section-bg">
+      <div className="container" data-aos="fade-up">
+        <SectionTitle title=" OurMenu" subtitle="Check Our Tasty Menu" />
+
+        <div className="row" data-aos="fade-up" data-aos-delay="100">
+          <div className="col-lg-12 d-flex justify-content-center">
+            <ul id="menu-flters">
+              {filters.map((filter) => (
+                <li
+                  key={filter.id}
+                  className={`${filter.active ? "filter-active" : undefined}`}
+                  onClick={() => handleFilterChange(filter.id, filter.category)}
+                >
+                  {filter.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div
+          className="row menu-container"
+          data-aos="fade-up"
+          data-aos-delay="200"
+        >
+          {!items ? (
+            <Preloader />
+          ) : items.length > 0 ? (
+            items.map(
+              (item: {
+                id: number;
+                name: string;
+                // preview: string;
+                price: number;
+              }) => <MenuItem key={item.id} item={item} />
+            )
+          ) : (
+            <Preloader />
+          )}
+        </div>
+      </div>
+    </section>
+  );
 }
