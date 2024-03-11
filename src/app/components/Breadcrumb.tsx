a44c2ab53146673e42e4fde1063d19d7cbdafe09
justin <justin.ng26@hotmail.com>
2024-03-11 18:49:01 +0000

    added breadcrumb and started menu card

---
+++ src/app/components/Breadcrumb.tsx
@@ -0,0 +1,22 @@
+import "../styles/breadcrumb.css";
+import Link from "next/link";
+
+export default function Breadcrumb({ page }: { page: string }) {
+  return (
+    <section className="breadcrumbs">
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center">
+          <h2>{page}</h2>
+          <ol>
+            <li>
+              <Link href="/">
+                <i className="bi bi-house-door-fill"></i>
+              </Link>
+            </li>
+            <li>{page}</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/menu/[id]/page.tsx
+++ src/app/menu/[id]/page.tsx
@@ -1,3 +1,47 @@
-export default function page() {
-  return <div>page</div>;
+import Breadcrumb from "@/app/components/Breadcrumb";
+import Image from "next/image";
+
+async function getFoodData(id: string) {
+  const res = await fetch(`http://localhost:3000/api/menu/${id}`);
+  return res.json();
+}
+
+export default async function MenuCard({ params }: { params: { id: string } }) {
+  const id = params.id;
+
+  const food = await getFoodData(id);
+
+  return (
+    <main id="main">
+      <Breadcrumb page="Menu" />
+
+      <section className="inner-page">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <Image
+                src={food.preview}
+                alt="food item"
+                height={500}
+                width={500}
+                className="img-fluid"
+              />
+            </div>
+
+            <div className="col-lg">
+              <h2 className="mt-3">{food.name}</h2>
+              <h4 className="mt-2">
+                <i>{food.options}</i>
+              </h4>
+
+              <h4>
+                <i>{food.allergy}</i>
+              </h4>
+              <p className="m5-5">{food.description}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
