a9a1f7cfbdd73cd8d006ca31988e917f288bc95c
justin <justin.ng26@hotmail.com>
2024-03-16 20:18:48 +0000

    installed resend and testing email booking submisison

--- src/app/actions/sendBooking.ts
+++ src/app/actions/sendBooking.ts
@@ -1,12 +1,22 @@
 "use server";
 
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.BOOKING_API_KEY);
+
 export const sendBooking = async (formData: FormData) => {
-  console.log("running on server");
-  console.log(formData.get("name"));
-  console.log(formData.get("email"));
-  console.log(formData.get("message"));
-  console.log(formData.get("people"));
-  console.log(formData.get("phone"));
-  console.log(formData.get("time"));
-  console.log(formData.get("date"));
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const message = formData.get("message");
+  const people = formData.get("people");
+  const phone = formData.get("phone");
+  const time = formData.get("time");
+  const date = formData.get("date");
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "justinng878@gmail.com",
+    subject: "New booking",
+    text: "test, hello world!",
+  });
 };
