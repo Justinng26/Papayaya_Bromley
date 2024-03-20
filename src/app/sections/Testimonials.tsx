f2d98d282ada99af261ebe93df73f1a1e0c311af
justin <justin.ng26@hotmail.com>
2024-03-20 13:34:56 +0000

    added testimonials from google

--- src/app/sections/Testimonials.tsx
+++ src/app/sections/Testimonials.tsx
@@ -53,7 +53,7 @@ export default function Testimonials() {
               },
 
               1200: {
-                slidesPerView: 3,
+                slidesPerView: 2,
                 spaceBetween: 20,
               },
             }}
