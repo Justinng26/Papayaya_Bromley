f12d23f219ca7dd2144f746f8cbaecf3d4117d24
justin <justin.ng26@hotmail.com>
2024-05-14 18:09:22 +0100

    prepare for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import About from "./sections/About";
 import Hero from "./sections/Hero";
-// import WhyUs from "./sections/WhyUs";
 import Menu from "./sections/Menu";
-// import Events from "./sections/Events";
-// import BookAtable from "./sections/BookAtable";
 import Testimonials from "./sections/Testimonials";
 import Gallery from "./sections/Gallery";
 import OpeningHours from "./sections/OpeningHours";
@@ -17,13 +14,9 @@ export default function Home() {
       <Hero />
       <main id="main">
         <About />
-        {/* <WhyUs /> */}
         <Menu />
-        {/* <Events /> */}
-
         <Gallery />
         <Booking />
-        {/* <BookAtable /> */}
         <OpeningHours />
         <Testimonials />
         <Contact />
