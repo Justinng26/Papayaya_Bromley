c16703c6e6d7d3e2669976a278cc18fe3b3035ee
justin <justin.ng26@hotmail.com>
2024-05-29 12:38:29 +0100

    menu and gallery still stopping deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import About from "./sections/About";
 // import Hero from "./sections/Hero";
-// import Menu from "./sections/Menu";
+import Menu from "./sections/Menu";
 // import Testimonials from "./sections/Testimonials";
-import Gallery from "./sections/Gallery";
+// import Gallery from "./sections/Gallery";
 import OpeningHours from "./sections/OpeningHours";
 import Contact from "./sections/Contact";
 import Booking from "./sections/Booking";
 import dynamic from "next/dynamic";
-// import { BASE_API_URL } from "./utils/constants";
 
 const DynamicHero = dynamic(() => import("./sections/Hero"), {
   ssr: false,
@@ -22,8 +21,8 @@ export default function Home() {
       <DynamicHero />
       <main id="main">
         <About />
-        {/* <Menu /> */}
-        <Gallery />
+        <Menu />
+        {/* <Gallery /> */}
         <Booking />
         <OpeningHours />
         {/* <Testimonials /> */}
